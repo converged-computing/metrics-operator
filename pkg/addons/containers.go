470bb6c5ef5307b8dd779786e4f7b4e3864374f8
vsoch <vsoch@users.noreply.github.com>
2023-11-16 10:17:40 -0700

    fix
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- pkg/addons/containers.go
+++ pkg/addons/containers.go
@@ -62,10 +62,6 @@ func (a *ApplicationAddon) Validate() bool {
 	return true
 }
 
-func (a ApplicationAddon) AssembleInitContainers() []specs.ContainerSpec {
-	return []specs.ContainerSpec{}
-}
-
 // AssembleContainers adds the addon application container
 func (a ApplicationAddon) AssembleContainers() []specs.ContainerSpec {
 	return []specs.ContainerSpec{{
